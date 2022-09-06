ae9ede48b92b56b1da7acc56b94595bfac37a9a5
W1ngshot <kirill-samsonov@bk.ru>
2022-09-06 15:37:52 +0300

    homework1 done

--- HomeWork1/Hw1/Parser.cs
+++ HomeWork1/Hw1/Parser.cs
@@ -7,13 +7,24 @@ public static class Parser
         out CalculatorOperation operation, 
         out double val2)
     {
-        throw new NotImplementedException();
+        if (!IsArgLengthSupported(args))
+            throw new ArgumentException("Args length must be 3");
+        operation = ParseOperation(args[1]);
+        if (!(double.TryParse(args[0], out val1) && double.TryParse(args[2], out val2)))
+            throw new ArgumentException();
     }
 
     private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
 
     private static CalculatorOperation ParseOperation(string arg)
     {
-        throw new NotImplementedException();
+        return arg switch
+        {
+            "+" => CalculatorOperation.Plus,
+            "-" => CalculatorOperation.Minus,
+            "*" => CalculatorOperation.Multiply,
+            "/" => CalculatorOperation.Divide,
+            _ => throw new InvalidOperationException()
+        };
     }
 }
