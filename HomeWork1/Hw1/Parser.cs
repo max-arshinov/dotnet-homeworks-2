a43f0d6db5ea66d30d7c2dade9e2d57bd551c6bf
Nail Zinnurov <nailzinn122033@gmail.com>
2022-09-07 22:40:11 +0300

    parser done

--- HomeWork1/Hw1/Parser.cs
+++ HomeWork1/Hw1/Parser.cs
@@ -7,13 +7,33 @@ public static class Parser
         out CalculatorOperation operation, 
         out double val2)
     {
-        throw new NotImplementedException();
+        if (!IsArgLengthSupported(args))
+            throw new ArgumentException();
+
+        val1 = ParseValue(args[0]);
+        val2 = ParseValue(args[2]);
+        operation = ParseOperation(args[1]);
+
+        if (operation == CalculatorOperation.Undefined)
+            throw new InvalidOperationException();
     }
 
     private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
 
+    private static double ParseValue(string arg)
+    {
+        return double.TryParse(arg, out double val) ? val : throw new ArgumentException();
+    }
+
     private static CalculatorOperation ParseOperation(string arg)
     {
-        throw new NotImplementedException();
+        return arg switch
+        {
+            "+" => CalculatorOperation.Plus,
+            "-" => CalculatorOperation.Minus,
+            "*" => CalculatorOperation.Multiply,
+            "/" => CalculatorOperation.Divide,
+            _ => CalculatorOperation.Undefined
+        };
     }
 }
