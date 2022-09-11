4402149532b43128b1031f957c618358e3a6f8a7
RusLoneWorker <60140895+RusPyAloneWorker@users.noreply.github.com>
2022-09-11 14:56:24 +0300

    Update Parser.cs

--- HomeWork1/Hw1/Parser.cs
+++ HomeWork1/Hw1/Parser.cs
@@ -1,19 +1,35 @@
+using System;
+
 namespace Hw1;
 
-public static class Parser
-{
-    public static void ParseCalcArguments(string[] args, 
-        out double val1, 
-        out CalculatorOperation operation, 
-        out double val2)
+public class Parser
     {
-        throw new NotImplementedException();
-    }
+        public static void ParseCalcArguments(string[] args, 
+            out double val1, 
+            out CalculatorOperation operation, 
+            out double val2)
+        {
+            if (!IsArgLengthSupported(args))
+                throw new ArgumentException();
+            if (!Double.TryParse(args[0], out val1))
+                throw new ArgumentException();
+            if (!Double.TryParse(args[2], out val2))
+                throw new ArgumentException();
+            operation = ParseOperation(args[1]);
+        }
 
-    private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
+        private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
 
-    private static CalculatorOperation ParseOperation(string arg)
-    {
-        throw new NotImplementedException();
+        private static CalculatorOperation ParseOperation(string arg)
+        {
+            var operation = arg switch
+            {
+                "+" => CalculatorOperation.Plus,
+                "-" => CalculatorOperation.Minus,
+                "/" => CalculatorOperation.Divide,
+                "*" => CalculatorOperation.Multiply,
+                _ => throw new InvalidOperationException()
+            };
+            return operation;
+        }
     }
-}
