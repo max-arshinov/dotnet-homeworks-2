0c677c3504a194f119eb0a62ab36b46ba5ed42a4
ValentineGrachyov <valentinegrachyov@gmail.com>
2022-09-12 01:06:46 +0300

    Update Parser.cs

--- Homework1/Hw1/Parser.cs
+++ Homework1/Hw1/Parser.cs
@@ -7,13 +7,24 @@ public static class Parser
         out CalculatorOperation operation, 
         out double val2)
     {
-        throw new NotImplementedException();
+        val1 = (double)Parser.ParseOperation(args[0]);
+        val2 = (double)Parser.ParseOperation(args[2]);
+
+        if (args.Length > 3) { throw new ArgumentException(); }
+
+        if (args[1] == "+") { operation = CalculatorOperation.Plus; }
+        else if (args[1] == "-") { operation = CalculatorOperation.Minus; }
+        else if (args[1] == "*") { operation = CalculatorOperation.Multiply; }
+        else if (args[1] == "/") { operation = CalculatorOperation.Divide; }
+        else throw new InvalidOperationException();
+
     }
 
     private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
 
+
     private static CalculatorOperation ParseOperation(string arg)
     {
-        throw new NotImplementedException();
+        return Enum.Parse<CalculatorOperation>(arg);
     }
 }
