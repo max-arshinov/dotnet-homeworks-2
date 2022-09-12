ecf8c0671597591013124c0069fee6dd09375b93
Guryanov Vadim <vadim.guryanov.2004@mail.ru>
2022-09-12 22:37:37 +0300

    hope4

--- HomeWork1/Hw1/Parser.cs
+++ HomeWork1/Hw1/Parser.cs
@@ -1,22 +1,25 @@
+using System.Collections.ObjectModel;
+
 namespace Hw1;
 
 public static class Parser
 {
-    public static void ParseCalcArguments(string[] args, 
+    public static void ParseCalcArguments(ReadOnlyCollection<string> args, 
         out double val1, 
         out CalculatorOperation operation, 
         out double val2)
     {
-        if(args.Length != 3)
-            throw new ArgumentException();
+        if(!IsArgLengthSupported(args))
+            throw new ArgumentException("Invalid count of input arguments");
         var val1Flag = double.TryParse(args[0], out val1);
         var val2Flag = double.TryParse(args[2], out val2);
-        if (!(val1Flag && val2Flag))
-            throw new ArgumentException();
         operation = ParseOperation(args[1]);
+        if (!(val1Flag && val2Flag))
+            throw new ArgumentException(
+                "Wrong request syntax or unsupported type of values given. Format: {value operation value}. As values can be entered all integer or fractional numbers");
     }
 
-    private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
+    private static bool IsArgLengthSupported(ReadOnlyCollection<string> args) => args.Count == 3;
 
     private static CalculatorOperation ParseOperation(string arg)
     {
@@ -31,7 +34,7 @@ public static class Parser
             case "/":
                 return CalculatorOperation.Divide;
             default:
-                throw new InvalidOperationException();
+                throw new InvalidOperationException("Unsupported operation given");
         }
     }
 }
