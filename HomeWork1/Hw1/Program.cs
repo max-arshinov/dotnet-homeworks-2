bb995bd3c2ed05bf9de41c4834058dd9d3dad61c
Adel Mukhamedshin <78945qw@gmail.com>
2022-09-12 17:49:44 +0300

    Дополнил ДЗ

--- HomeWork1/Hw1/Program.cs
+++ HomeWork1/Hw1/Program.cs
@@ -1,7 +1,4 @@
-﻿var arg1 = args[0];
-var operation = args[1];
-var arg2 = args[2];
+﻿using Hw1;
 
-// TODO: implement calculator logic
-var result = arg1 + operation + arg2;
-Console.WriteLine(result);
+Parser.ParseCalcArguments(args, out var left, out var op, out var right);
+Console.WriteLine(Calculator.Calculate(left, op, right));
