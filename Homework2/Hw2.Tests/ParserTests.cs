202a9f5ddf3a68837ee4c291009a975eaac57046
Egor <korolkov-nat@mail.ru>
2022-09-19 00:01:38 +0300

    unresolved problem with test

--- Homework2/Hw2.Tests/ParserTests.cs
+++ Homework2/Hw2.Tests/ParserTests.cs
@@ -12,7 +12,16 @@ namespace Hw2Tests
         [InlineData("/", CalculatorOperation.Divide)]
         public void TestCorrectOperations(string operation, CalculatorOperation operationExpected)
         {
-            throw new NotImplementedException();
+            // arrange
+            var args = new[] { "15", operation, "5" };
+            
+            //act
+            Parser.ParseCalcArguments(args, out var val1, out var operationResult, out var val2);
+
+            //assert
+            Assert.Equal(15, val1);
+            Assert.Equal(operationExpected, operationResult);
+            Assert.Equal(5, val2);
         }
         
         [Theory]
@@ -21,19 +30,31 @@ namespace Hw2Tests
         [InlineData("a", "+", "f")]
         public void TestParserWrongValues(string val1, string operation, string val2)
         {
-            throw new NotImplementedException();
+            // arrange
+            var args = new[] { val1, operation, val2 };
+            
+            //assert
+            Assert.Throws<ArgumentException>(() => Parser.ParseCalcArguments(args, out _, out _, out _));
         }
         
         [Fact]
         public void TestParserWrongOperation()
         {
-            throw new NotImplementedException();
+            // arrange
+            var args = new[] { "3", ".", "4" };
+            
+            //assert
+            Assert.Throws<InvalidOperationException>(() => Parser.ParseCalcArguments(args, out _, out _, out _));
         }
 
         [Fact]
         public void TestParserWrongLength()
         {
-            throw new NotImplementedException();
+            // arrange
+            var args = new[] { "3", ".", "4", "5" };
+            
+            //assert
+            Assert.Throws<ArgumentException>(() => Parser.ParseCalcArguments(args, out _, out _, out _));
         }
     }
 }
