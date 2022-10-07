a3779c39e3383c10f348358e41ed6e9d995e45cd
Адель <78945qw@gmail.com>
2022-10-08 02:57:42 +0300

    Entrypoint

--- Homework4/Hw4/Program.fs
+++ Homework4/Hw4/Program.fs
@@ -1,1 +1,7 @@
-﻿raise (System.NotImplementedException())
+﻿open Hw4
+
+[<EntryPoint>]
+let main args =
+    let res = Parser.parseCalcArguments args
+    printf $"%f{Calculator.calculate res.arg1 res.operation res.arg2}"
+    0 // return an integer exit code
