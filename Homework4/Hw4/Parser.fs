1f5489b3170986c0b980c2dd8ccc62d6135e2aea
Адель <78945qw@gmail.com>
2022-10-07 22:52:56 +0300

    Hw4

--- Homework4/Hw4/Parser.fs
+++ Homework4/Hw4/Parser.fs
@@ -3,18 +3,51 @@
 open System
 open Hw4.Calculator
 
+let (|Float|_|) (str: string) =
+    match System.Double.TryParse str with
+    | true, float -> Some float
+    | _ -> None
 
-type CalcOptions = {
-    arg1: float
-    arg2: float
-    operation: CalculatorOperation
-}
 
-let isArgLengthSupported (args : string[]) =
-    NotImplementedException() |> raise
+type CalcOptions =
+    { arg1: float
+      arg2: float
+      operation: CalculatorOperation }
 
-let parseOperation (arg : string) =
-    NotImplementedException() |> raise
-    
-let parseCalcArguments(args : string[]) =
-    NotImplementedException() |> raise
+let isArgLengthSupported (args: string []) =
+    match args.Length with
+    | 3 -> true
+    | _ -> false
+
+let parseOperation (arg: string) =
+    match arg with
+    | "+" -> CalculatorOperation.Plus
+    | "-" -> CalculatorOperation.Minus
+    | "*" -> CalculatorOperation.Multiply
+    | "/" -> CalculatorOperation.Divide
+    | _ -> ArgumentException "Invalid operation" |> raise
+
+let parseCalcArguments (args: string []) =
+    if not (isArgLengthSupported args) then
+        ArgumentException "Invalid arguments count"
+        |> raise
+    else
+        let arg1 =
+            match args.[0] with
+            | Float float -> float
+            | _ ->
+                ArgumentException "Invalid first argument"
+                |> raise
+
+        let operation = parseOperation args.[1]
+
+        let arg2 =
+            match args.[2] with
+            | Float float -> float
+            | _ ->
+                ArgumentException "Invalid second argument"
+                |> raise
+
+        { arg1 = arg1
+          arg2 = arg2
+          operation = operation }
