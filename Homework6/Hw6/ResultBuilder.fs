8d0dbe1aaf7726ed4bf99d8db03e6d1de09463e9
Адель <78945qw@gmail.com>
2022-10-22 19:02:04 +0300

    удалил строку

--- Homework6/Hw6/ResultBuilder.fs
+++ Homework6/Hw6/ResultBuilder.fs
@@ -5,7 +5,6 @@ type ResultBuilder() =
         match a with
         | Ok a -> f a
         | Error e -> Error e
-    member builder.Return x : Result<'a, 'b> = Ok x
     member builder.ReturnFrom x : Result<'a, 'b> = x
 
 let result = ResultBuilder()
