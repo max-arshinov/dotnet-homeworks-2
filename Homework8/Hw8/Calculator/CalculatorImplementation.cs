2ff260931e6c50973faa7a49ccf05dae2859be27
Адель <78945qw@gmail.com>
2022-10-31 14:59:32 +0300

    HW8

---
+++ Homework8/Hw8/Calculator/CalculatorImplementation.cs
@@ -0,0 +1,26 @@
+﻿namespace Hw8.Calculator;
+
+public class CalculatorImplementation : ICalculator
+{
+    public double Plus(double val1, double val2)
+    {
+        return val1 + val2;
+    }
+
+    public double Minus(double val1, double val2)
+    {
+        return val1 - val2;
+    }
+
+    public double Multiply(double val1, double val2)
+    {
+        return val1 * val2;
+    }
+
+    public double Divide(double firstValue, double secondValue)
+    {
+        if (secondValue == 0) 
+            throw new InvalidOperationException(Messages.InvalidOperationMessage);
+        return firstValue / secondValue;
+    }
+}
