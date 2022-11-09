d7145a89962a7be584bc5fa0acaec314ae8fb1f6
max arshinov <max.arshinov@gmail.com>
2022-11-09 17:22:04 +0100

    Homework 10

---
+++ Homework10/Hw10.Tests/UserMessagerForTest.cs
@@ -0,0 +1,12 @@
+namespace Homework10.Tests;
+
+public static class UserMessagerForTest
+{
+    public static string WaitingTimeIsLess(long minExpectedTime, long executionTime) =>
+        $@"Время подсчета меньше ожидаемого.
+           Мин время: {minExpectedTime}, актуальное время: {executionTime}";
+
+    public static string WaitingTimeIsMore(long maxExpectedTime, long executionTime) =>
+        $@"Время подсчета больше ожидаемого.
+           Макс время: {maxExpectedTime}, актуальное время: {executionTime});";
+}
