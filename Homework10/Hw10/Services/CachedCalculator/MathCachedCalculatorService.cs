9966a0bf245f874696645585981f1a275b92f0ff
Адель <78945qw@gmail.com>
2022-11-14 15:11:33 +0300

    Hw10 - done

--- Homework10/Hw10/Services/CachedCalculator/MathCachedCalculatorService.cs
+++ Homework10/Hw10/Services/CachedCalculator/MathCachedCalculatorService.cs
@@ -1,6 +1,7 @@
 ﻿using Hw10.DbModels;
 using Hw10.Dto;
 using Hw10.Services.MathCalculator;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hw10.Services.CachedCalculator;
 
@@ -17,6 +18,28 @@ public class MathCachedCalculatorService : IMathCalculatorService
 
 	public async Task<CalculationMathExpressionResultDto> CalculateMathExpressionAsync(string? expression)
 	{
-		throw new NotImplementedException();
+		var cached = await _dbContext.SolvingExpressions.FirstOrDefaultAsync(x => 
+			x.Expression == expression);
+		
+		if(cached != null)
+			return new CalculationMathExpressionResultDto
+			{
+				IsSuccess = true,
+				Result = cached.Result
+			};
+
+		var newCalculation = await _simpleCalculator.CalculateMathExpressionAsync(expression);
+		
+		if (newCalculation.IsSuccess)
+		{
+			await _dbContext.SolvingExpressions.AddAsync(new SolvingExpression
+			{
+				Expression = expression,
+				Result = newCalculation.Result
+			});
+			await _dbContext.SaveChangesAsync();
+		}
+
+		return newCalculation;
 	}
 }
