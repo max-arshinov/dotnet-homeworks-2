ac8e9f5c896f3d18bfaed2821139d0990993bb67
JosephMonteCarlo <DioBrando2003@yandex.ru>
2022-12-11 16:11:55 +0300

    Альметов 2.1.7

---
+++ Homework7/Hw7/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Hw7.Models.ForTests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hw7.Controllers;
+
+public class TestController : Controller
+{
+    [HttpGet]
+    public IActionResult TestModel() => View();
+
+    [HttpPost]
+    public IActionResult TestModel(TestModel model) => View(model);
+}
--- Homework7/Hw7/MyHtmlServices/HtmlHelperExtensions.cs
+++ Homework7/Hw7/MyHtmlServices/HtmlHelperExtensions.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Hw7.MyHtmlServices;
 
@@ -7,6 +11,61 @@ public static class HtmlHelperExtensions
 {
     public static IHtmlContent MyEditorForModel(this IHtmlHelper helper)
     {
-        throw new NotImplementedException();
+        var model = helper.ViewData.Model;
+        var properties = helper.ViewData.ModelMetadata.ModelType.GetProperties();
+        return ConstructHtml(model, properties);
     }
-} 
+
+    private static HtmlContentBuilder ConstructHtml(object? model, IEnumerable<PropertyInfo> properties)
+    {
+        var page = new HtmlContentBuilder();
+        foreach (var property in properties)
+        {
+            page.AppendHtmlLine(
+                "<div>" + GetPropertyField(property) + GetInputField(model, property) + "</div>");
+        }
+        return page;
+    }
+
+    private static string GetPropertyField(MemberInfo property)
+    {
+        var attribute = property.GetCustomAttribute<DisplayAttribute>();
+        var attributeName = attribute is null
+            ? string.Join(" ", Regex.Split(property.Name, "(?<!^)(?=[A-Z])"))
+            : attribute.Name!;
+        return $"<label for=\"{property.Name}\">{attributeName}</label><br>";
+    }
+
+    private static string GetInputField(object? model, PropertyInfo property)
+    {
+        var sb = new StringBuilder();
+        var modelValue = model is null ? string.Empty : $" value=\"{property.GetValue(model)}\"";
+        if (property.PropertyType.IsEnum)
+        {
+            sb.AppendLine($"<select" + modelValue + ">");
+            foreach (var val in Enum.GetValues(property.PropertyType))
+            {
+                sb.AppendLine($"<option>{val}</option>");
+            } 
+            sb.AppendLine("</select>");
+        }
+        else
+        {
+            var inputType = property.PropertyType == typeof(int) ? "number" : "text";
+            sb.AppendLine($"<input id=\"{property.Name}\" type=\"{inputType}\"" + modelValue + "/>");
+        }
+
+        if (model is not null)
+            sb.AppendLine(GetErrorSpan(property, property.GetValue(model)));
+        return sb.ToString();
+    }
+
+    private static string GetErrorSpan(MemberInfo property, object? value)
+    {
+        var validationAttributes = property.GetCustomAttributes<ValidationAttribute>();
+        foreach (var attribute in validationAttributes)
+            if (!attribute.IsValid(value))
+                return $"<div>{attribute.ErrorMessage!}</div>";
+        return string.Empty;
+    }
+}
